ed46ce7e7a1f1363b72cc0eba2cc10a703d5f23c
Alex Cândido <alex.candido.tec@gmail.com>
2022-08-22 20:39:40 -0300

    estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,56 @@
+import { FaChevronRight } from 'react-icons/fa';
 import logoImg from '../../assets/logo.svg';
-import { Form, Title } from './styles';
+import { Form, Repositories, Title } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
+
       <Form>
         <input type="text" placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/80834340?v=4"
+            alt="Alex Candido"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FaChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/80834340?v=4"
+            alt="Alex Candido"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FaChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/80834340?v=4"
+            alt="Alex Candido"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FaChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
